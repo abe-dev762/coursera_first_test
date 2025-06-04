a226a84587e754201d1e2d3c2563ac09a202838c
Ahmad Zaenal Abidin <ahmadabidin762.dev@gmail.com>
2025-06-04 10:06:14 +0800

    test: test function calculate area;

--- index.js
+++ index.js
@@ -1,50 +1,10 @@
-let userRole = "admin";
-let accessLevel;
+let length;
+let width;
 
-if (userRole === "admin") {
-    accessLevel = "Full access granted";
-    console.log("Access Level:", accessLevel);
-} else if (userRole === "manager") {
-    accessLevel = "Limited access granted";
-    console.log(accessLevel);
-} else {
-    accessLevel = "Access denied";
-    console.log(accessLevel);
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+    let area = length * width;
+
+    result = document.getElementById('result').innerText = `the area of the rectangle is: ${area}`;
 }
-
-let isLoggedIn = true;
-let userMessage;
-
-if (isLoggedIn) {
-    if (userRole === "admin") {
-        userMessage = "Welcome, Admin!";
-    }
-    else {
-        userMessage = "Welcome, Manager!";
-    }
-} else {
-    userMessage = "Please login to access the system";
-}
-console.log("User Message:", userMessage);
-
-let userType = "subscriber";
-let userCategory;
-
-switch (userType) {
-    case "admin":
-        userCategory = "Administrator";
-        break;
-     case "manager":
-        userCategory = "Manager";
-        break;
-    case "subscriber":
-        userCategory = "Subscriber";
-        break;
-    case "default":
-        userCategory = "Unkown";
-}
-console.log("User Category:", userCategory)
-
-let isAuthenticated = true;
-let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
-console.log("Authentication Status:", authenticationStatus)
