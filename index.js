8f55ceb5a55a266d6bb99e0e7a052f6c16852ad0
Ahmad Zaenal Abidin <ahmadabidin762.dev@gmail.com>
2025-06-06 20:51:34 +0800

    test: testing array;

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-function submitFeedback() {
-    alert('Thank you for your feedback');
-document.getElementById('userName').innerHTML = username;
-document.getElementById('UserAge').innerHTML = age;
-document.getElementById('userEmail').innerHTML = email;
-document.getElementById('UserJob').innerHTML = job;
-document.getElementById('Userdesignation').innerHTML = designation;
-document.getElementById('userProductType').innerHTML = productType;
-document.getElementById('userFeedback').innerHTML = feedback;
-document.getElementById('userInfo').style.display = 'block';
-}
- 
-const name = document.getElementById('name').value;
- const age = document.getElementById('age').value;
- const email = document.getElementById('email').value;
- const job = document.getElementById('job').value;
- const designation = document.getElementById('designation').value;
- const productType = document.getElementById('productType').value;
- const feedbackText = document.getElementById('feedbackText').value;
- const submitButton = document.getElementById('sumbitBtn');
+const breakFastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
- submitButton.onclick = submitFeedback();
+const breakFastMenuItemsHTML = breakFastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+    document.getElementById('breakFastMenuItems').innerHTML = breakFastMenuItemsHTML;
 
- document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        submitFeedback();
-    }
- })
+    let mainCourseItem = '';
+    mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+    document.getElementById('mainCourseMenu').innerHTML = mainCourseItem;
+
+    let dessertItem = '';
+    for (i = 0; i < dessertMenu.length; i++) {
+        dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
